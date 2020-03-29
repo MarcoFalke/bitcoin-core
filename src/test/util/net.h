90b577629e92d6e055630d56ea8a76e906c10785
MarcoFalke <falke.marco@gmail.com>
2020-03-29 22:49:32 +0200

    net: Make stale tip check time type-safe, extend test
    * GetTime is the non-type-safe and deprecated version of GetTime<>
    * Extend test with ASSERT_DEBUG_LOG
    * Extend test with case where a block is in flight from a node that is
      about to be evicted

--- src/test/util/net.h
+++ src/test/util/net.h
@@ -8,6 +8,7 @@
 #include <compat.h>
 #include <net.h>
 #include <util/sock.h>
+#include <netmessagemaker.h>
 
 #include <cassert>
 #include <cstring>
@@ -34,6 +35,15 @@ struct ConnmanTestMsg : public CConnman {
     void NodeReceiveMsgBytes(CNode& node, Span<const uint8_t> msg_bytes, bool& complete) const;
 
     bool ReceiveMsgFrom(CNode& node, CSerializedNetMsg& ser_msg) const;
+
+    template <typename P>
+    void ReceiveMsgFrom(CNode& node, const std::string& type, const P& payload)
+    {
+        const CNetMsgMaker msg_maker(PROTOCOL_VERSION);
+        CSerializedNetMsg m_ser = msg_maker.Make(type.c_str(), payload);
+
+        assert(ReceiveMsgFrom(node, m_ser));
+    }
 };
 
 constexpr ServiceFlags ALL_SERVICE_FLAGS[]{
